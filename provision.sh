94fc6d59f8df67098cc76dc0bfad8294319e0bca
helverton <helvertonsantos@gmail.com>
2017-01-21 23:23:34 -0200

    update provision

--- provision.sh
+++ provision.sh
@@ -56,8 +56,7 @@ sudo ./bin/kibana
 
 # install logstash
 echo 'deb http://packages.elasticsearch.org/logstash/2.0/debian stable main' | sudo tee /etc/apt/sources.list.d/logstash.list
-sudo apt-get update
-sudo apt-get install logstash
+sudo apt-get update && sudo apt-get install logstash
 
 # install head
 cd /usr/share/elasticsearch/bin/ 
@@ -68,3 +67,65 @@ cd
 
 # intall odbc
 sudo apt-get -y install unixodbc unixodbc-dev freetds-dev freetds-bin tdsodbc
+
+
+
+
+#==============================
+
+# # install grafana
+# echo "Installing Grafana repository..."
+# wget -qO - https://packagecloud.io/gpg.key | sudo apt-key add -
+# echo "deb https://packagecloud.io/grafana/stable/debian/ jessie main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+# sudo apt-get update && sudo apt-get install grafana
+
+# sudo iptables -I INPUT -p tcp --dport 3000 -j ACCEPT
+# sudo iptables-save
+
+# sudo update-rc.d grafana-server defaults 95 10
+# sudo chmod +x /etc/init.d/grafana-server
+# sudo /etc/init.d/grafana-server start
+
+
+
+# # config integration between logstash and sql server
+# cd /opt/logstash
+# sudo ./bin/plugin install logstash-input-jdbc
+# cd
+# sudo wget https://download.microsoft.com/download/0/2/A/02AAE597-3865-456C-AE7F-613F99F850A8/enu/sqljdbc_6.0.7728.100_enu.tar.gz
+
+# gunzip sqljdbc_6.0.7728.100_enu.tar.gz
+# rm -rf sqljdbc_6.0.7728.100_enu.tar.gz
+
+# tar -xvf sqljdbc_6.0.7728.100_enu.tar
+# rm -rf sqljdbc_6.0.7728.100_enu.tar
+
+# sudo mkdir /opt/logstash/lib/jdbc
+# sudo cp sqljdbc_6.0/enu/sqljdbc42.jar /opt/logstash/lib/jdbc/
+
+# cd /opt/logstash
+# sudo su
+# > sql.conf
+# echo "input {
+#   jdbc {
+#     jdbc_driver_library => "/opt/logstash/lib/jdbc/sqljdbc42.jar"
+#     jdbc_driver_class => "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+#     jdbc_connection_string => "jdbc:sqlserver://ip_sql_server;user=***;password=*****;"
+#     jdbc_user => "***"
+#     jdbc_password => "*****"
+#     schedule => "*/5 * * * * *"
+#     statement => "select * from [db].[schema].[table]"
+#   }
+# }
+# filter {
+# }
+# output {
+#   elasticsearch {
+#     hosts => "ip_elasticsearch"
+#     index => "index_name"
+#     document_type => "type_name"
+#     document_id => "%{id}"
+#     manage_template => true
+#   }
+#   stdout { codec => rubydebug }
+# }" >> sql.conf
